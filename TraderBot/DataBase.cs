89a2babed6f3e0ff5e53531d180ce16cd2f0d010
unknown <rodrigolucasmagenis@gmail.com>
2018-08-18 13:38:55 -0300

    Adicionada percistência com banco de dados SQL server. refs TB03

---
+++ TraderBot/DataBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraderBot
+{
+    public class DataBase
+    {
+        SqlConnection con = new SqlConnection();
+
+        public void InsertQuotation (String cdCurrency, String dtQuotation, String price)
+        {
+            string queryComand = "INSERT INTO PRICE ( CDCURRENCY, DTQUOTATION, VLPRICE ) VALUES ( " + cdCurrency + " , " + dtQuotation + " , " + price + " );";
+            string connectionString = GetConnectionString();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(queryComand, connection);
+                try
+                {
+                    connection.Open();
+                    sqlCommand.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    throw(ex);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+
+            string GetConnectionString()
+            {
+                return "Data Source=(local); Initial Catalog=TraderBotDB; Integrated Security=SSPI;";
+            }
+        }
+    }
+}
--- TraderBot/Program.cs
+++ TraderBot/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -11,34 +13,34 @@ namespace TraderBot
     {
         static void Main(string[] args)
         {
-            List<String> currencyCod = new List<String>();
-            List<Decimal> price = new List<Decimal>();
-            List<DateTime> quotationSchedule = new List<DateTime>();
+            String currencyCod;
+            String price;
+            String date;
+
 
             using (StreamReader file = new StreamReader(@"c:\users\rodrigo magenis\downloads\db_cotacoes - db_moedas_cotacoes (7).csv"))
             {
+                DataBase dataBase = new DataBase();
+
                 while (!file.EndOfStream)
                 {
                     string line = file.ReadLine();
                     string[] values = line.Split(',');
 
-                    if (values[0] != "NA") // se o código da moeda existir
+                    if (values[0] != "NA" && values[1] != "NA") // se o código da moeda existir
                     {
-                        string[] currencyValues = values[1].Split('.'); //separa o valor da moeda por pontos
+                        currencyCod = values[0]; // codigo da moeda
 
-                        currencyCod.Add(values[0]);
-                        price.Add(Convert.ToDecimal(currencyValues[0] + "," + currencyValues[1])); //Concatena os inteiros com as partes inserindo virgula no meio
-                        quotationSchedule.Add(DateTime.Now);
+                        date = ( (DateTime.UtcNow) - (Convert.ToDateTime("01/01/2000 00:00:00") ) ).TotalSeconds.ToString(); // pega o total de segundos da data atual
+                        string[] dateValues = date.Split(',');
+                        date = dateValues[0]; // apenas o valor inteiro dos segundos
+
+                        price = values[1];
+
+                        dataBase.InsertQuotation(currencyCod, date, price);
                     }
                 }
             }
-
-            for (int i = 0; i < currencyCod.Count; i++)
-            {
-                Console.WriteLine("código = " + currencyCod[i] + " valor = " + price[i] + " horário = " + quotationSchedule[i].ToString() + "\n");
-            }
-
-            Console.ReadKey();
         }
     }
 }
